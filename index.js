ec92cbb6a64dcfb69bce4f9e06288e8868d491b0
unknown <igorpyles@gmail.com>
2021-05-25 20:29:55 -0300

    Novos itens na pagina do ticker

--- index.js
+++ index.js
@@ -16,12 +16,22 @@ app.get("/",(req, res) => {
     res.render("index");
 });
 
-app.post("/ticker",(req, res) => {
+app.post("/ticker", (req, res) => {
     var ativo = req.body.Ticker.toUpperCase();
-        res.render("ticker", {
-            ativo: ativo
-        });
+    var url = "https://api.hgbrasil.com/finance/stock_price?&fields=only_results&key=58db6e68&symbol=" + ativo;
+    axios.get(url).then(resp => {
+        var cotacao =  resp.data[ativo];
+        if(cotacao.symbol != ativo){  
+            res.redirect("/");
+        }else{
+            res.render("ticker", {
+                cotacao: cotacao,
+                ativo: ativo
+            });
+        };
+    });
 });
+
 
 app.listen(8080, () => {
     console.log("app rodando!");
